ded3fac95e211a1a01a79fc8970f131c6afed2f7
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-03-09 10:04:35 +0200

    Add users

---
+++ controllers/user.js
@@ -0,0 +1,26 @@
+var User = require('../models/user')
+
+// Create endpoint /api/users for POST
+exports.postUsers = function(req, res) {
+    var user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    user.save(function(err) {
+        if (err)
+            res.status(400).send(err)
+
+        res.status(201).json({message: 'User added OK'})
+    })
+}
+
+// Create endpoint /api/users for GET
+exports.getUsers = function(req, res) {
+    User.find(function(err, users) {
+        if (err)
+            res.send(err)
+
+        res.status(200).json(users)
+    })
+}
